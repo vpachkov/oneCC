abfc86c9457b1ea1d653f595533e710d8c704920
Plunkerusr <slava.pach@gmail.com>
2020-05-08 20:55:43 +0300

    Exceptions: Create exceptions for ArgsParser

--- Exceptions.h
+++ Exceptions.h
@@ -2,16 +2,35 @@
 #include <exception>
 
 namespace oneCC::Exceptions {
+
 class BadArgumentKey : public std::exception {
+public:
+    std::string message;
+    BadArgumentKey(const char* mes = NULL)
+    {
+        message = mes == NULL ? "" : std::string(mes);
+    }
+
+private:
     virtual const char* what() const throw()
     {
-        return "Argument key should start with - or --";
+        return ("Argument key should start with - or --, got " + message + " instead").c_str();
     }
 };
+
 class MissingArgument : public std::exception {
+public:
+    std::string message;
+    MissingArgument(const char* mes = NULL)
+    {
+        message = mes == NULL ? "" : std::string(mes);
+    }
+
+private:
     virtual const char* what() const throw()
     {
-        return "Missing required argument";
+        return ("Missing required argument, " + message).c_str();
     }
 };
+
 }
