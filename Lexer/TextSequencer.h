d397c94b1868823b83edc48b6d4758cf7f9c518d
nimelehin <nimelehin@gmail.com>
2020-05-11 20:52:02 +0300

    Lexer: Add support for _ ans $ in identifiers

--- Lexer/TextSequencer.h
+++ Lexer/TextSequencer.h
@@ -15,8 +15,10 @@ protected:
 
     bool isNextDigit() { return isdigit(lookupChar()); }
     bool isNextAlpha() { return isalpha(lookupChar()); }
+    bool isNextWordCapableSymbol() { return isNextAlpha() || isNextIdentifierAllowPunct(); }
     bool isNextHex() { return isHex(lookupChar()); }
-    bool isNextPunct() { return ispunct(lookupChar()); }
+    bool isNextPunct() { return isPunct(lookupChar()); }
+    bool isNextIdentifierAllowPunct() { return isIdentifierAllowPunct(lookupChar()); }
     bool isNextEOF() { return lookupChar() == EOF; }
     bool isNextSpace() { return lookupChar() == ' '; }
     bool isNextNL() { return lookupChar() == '\n'; }
@@ -28,6 +30,8 @@ protected:
 
 private:
     bool isHex(char c) { return ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F'); }
+    bool isPunct(char c) { return ispunct(c) && !isIdentifierAllowPunct(c); }
+    bool isIdentifierAllowPunct(char c) { return c == '_' || c == '$'; }
 
     std::shared_ptr<std::ifstream> m_fileStream;
     std::string m_currentLine;
