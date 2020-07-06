d6fdb8b7a65c804300919b01318c105d1feeb734
Plunkerusr <slava.pach@gmail.com>
2020-07-06 11:49:09 +0300

    Add base cmake building file

---
+++ cmake-build-debug/CMakeFiles/oneCC_executable.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/slava/Desktop/oneCC/oneCC/ArgsParser/ArgsParser.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/ArgsParser/ArgsParser.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Config/Config.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Config/Config.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Lexer/KeywordManager.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Lexer/KeywordManager.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Lexer/Lexer.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Lexer/Lexer.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Lexer/TextSequencer.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Lexer/TextSequencer.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Lexer/Token.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Lexer/Token.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Parser/Parser.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Parser/Parser.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Tests/ArgsParser/ArgsParserTest.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Tests/ArgsParser/ArgsParserTest.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Tests/Config/ConfigTest.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Tests/Config/ConfigTest.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/Tests/Lexer/LexerTest.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/Tests/Lexer/LexerTest.cpp.o"
+  "/Users/slava/Desktop/oneCC/oneCC/main.cpp" "/Users/slava/Desktop/oneCC/oneCC/cmake-build-debug/CMakeFiles/oneCC_executable.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ArgsParser"
+  "../Config"
+  "../Lexer"
+  "../Parser"
+  "../Tests"
+  "../Tests/ArgsParser"
+  "../Tests/Config"
+  "../Tests/Lexer"
+  "../Utils"
+  "../Utils/Debug"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/oneCC_executable.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oneCC_executable.dir/ArgsParser/ArgsParser.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Config/Config.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Lexer/KeywordManager.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Lexer/Lexer.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Lexer/TextSequencer.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Lexer/Token.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Parser/Parser.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Tests/ArgsParser/ArgsParserTest.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Tests/Config/ConfigTest.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/Tests/Lexer/LexerTest.cpp.o"
+  "CMakeFiles/oneCC_executable.dir/main.cpp.o"
+  "oneCC_executable"
+  "oneCC_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oneCC_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
