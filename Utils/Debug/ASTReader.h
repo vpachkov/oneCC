1cc9edeabdd693d9db1938e2f4a24f16baacbcdb
Plunkerusr <slava.pach@gmail.com>
2020-07-06 22:36:54 +0300

    Parser: add create integer variable support

--- Utils/Debug/ASTReader.h
+++ Utils/Debug/ASTReader.h
@@ -18,30 +18,52 @@ char tokenTypeToString(int tokenType)
         return '*';
     case oneCC::Lexer::TokenType::Divide:
         return '/';
+    case oneCC::Lexer::TokenType::Assign:
+        return '=';
     default:
         return '?';
     }
-    
+
 }
-void outputExpression(oneCC::AST::Node* expr, int depth = 0)
+void outputExpression(oneCC::Parser::GeneralExpression* expr, int depth = 0)
 {
     if (expr->type() == oneCC::AST::NodeType::Const) {
         auto* ptr = dynamic_cast<oneCC::AST::IntConstNode*>(expr);
         if (!ptr) {
             return;
         }
+    if (expr == NULL){
         std::cout << std::string(depth * 2, ' ');
-        std::cout << ptr->value();
+        std::cout << "undefined";
     }
-    if (expr->type() == oneCC::AST::NodeType::BinaryOperaion) {
-        auto* ptr = dynamic_cast<oneCC::AST::BinaryOperationNode*>(expr);
-        if (!ptr) {
-            return;
-        }
-        outputExpression(ptr->leftChild(), depth + 1);
+
+    if (expr->expressionType == oneCC::Parser::ExpressionType::Type ) {
         std::cout << std::string(depth * 2, ' ');
-        std::cout << tokenTypeToString(ptr->operation()) << std::endl;
-        outputExpression(ptr->rightChild(), depth + 1);
+        std::cout << expr->constToken.lexeme();
+    }
+
+    if (expr->expressionType == oneCC::Parser::ExpressionType::Identifier) {
+        std::cout << std::string(depth * 2, ' ');
+        std::cout << expr->constToken.lexeme();
+    }
+
+    if (expr->expressionType == oneCC::Parser::ExpressionType::Const) {
+        std::cout << std::string(depth * 2, ' ');
+        std::cout << expr->constToken.lexeme();
+    }
+    if (expr->expressionType == oneCC::Parser::ExpressionType::BinaryOperaion) {
+        outputExpression(expr->operands[0], depth + 1);
+        std::cout << std::string(depth * 2, ' ');
+        std::cout << tokenTypeToString(expr->operation) << std::endl;
+        outputExpression(expr->operands[1], depth + 1);
+    }
+
+    if (expr->expressionType == oneCC::Parser::ExpressionType::TernaryOperaion) {
+        outputExpression(expr->operands[0], depth + 1);
+        std::cout << std::string(depth * 2, ' ');
+        std::cout << tokenTypeToString(expr->operation) << std::endl;
+        outputExpression(expr->operands[1], depth + 1);
+        outputExpression(expr->operands[2], depth + 1);
     }
 
     std::cout << std::endl;
