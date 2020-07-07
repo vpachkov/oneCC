0ee522e70dadd9b30fab38810bf80df5b0ec1f61
Plunkerusr <slava.pach@gmail.com>
2020-07-07 22:47:43 +0300

    Parser: complete integer assign operation

--- Lexer/KeywordManager.cpp
+++ Lexer/KeywordManager.cpp
@@ -27,9 +27,13 @@ std::shared_ptr<KeywordManager> KeywordManager::makeStandart()
     man->addKeyword(Token("/", TokenType::Divide));
     man->addKeyword(Token("(", TokenType::OpenRoundBracket));
     man->addKeyword(Token(")", TokenType::CloseRoundBracket));
+    man->addKeyword(Token("=", TokenType::Assign));
 
     // Types
     man->addKeyword(Token("int", TokenType::TypeInt));
+
+    // Punctuation
+    man->addKeyword(Token(";", TokenType::EndOfStatement));
     return man;
 }
 
