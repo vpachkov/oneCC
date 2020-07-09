9979fef864a5823910346d31db3c9fb8ef2945a1
nimelehin <nimelehin@gmail.com>
2020-07-09 19:40:05 +0300

    Utils: Apply Webkit Style to ASTReader

--- Utils/Debug/ASTReader.h
+++ Utils/Debug/ASTReader.h
@@ -1,23 +1,25 @@
 #pragma once
-#include "../../Lexer/Token.h"
 #include "../../AST/ASTNode.h"
 #include "../../AST/Nodes/BinaryOperation.h"
 #include "../../AST/Nodes/IntConst.h"
-#include <iostream>
+#include "../../Lexer/Token.h"
 #include <cstdint>
+#include <iostream>
 #include <string>
 
 namespace oneCC::ASTUtils {
 
 class Visualizer {
 public:
-    
-    Visualizer() : m_saveTo("debug/astViz/") { };
-    Visualizer(std::string saveTo): m_saveTo(saveTo) {};
+    Visualizer()
+        : m_saveTo("debug/astViz/") {};
+    Visualizer(std::string saveTo)
+        : m_saveTo(saveTo) {};
 
     static char tokenTypeToString(int tokenType);
     void genDotDescriptor(AST::Node* node, std::string filename);
     void genTreePng(AST::Node* node);
+
 private:
     void startVisitingTree(AST::Node* node);
 
@@ -29,8 +31,8 @@ private:
     std::string toText(AST::IntConstNode*);
 
     int m_tin;
-    std::vector<std::vector<int>>m_children;
-    std::vector<std::string>m_labels;
+    std::vector<std::vector<int>> m_children;
+    std::vector<std::string> m_labels;
     std::string m_saveTo;
 };
 }
