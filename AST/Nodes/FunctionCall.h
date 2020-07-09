6421c822f7fb9dd3a453d5ffb41b46ca8ce1afa4
Plunkerusr <slava.pach@gmail.com>
2020-07-10 01:09:14 +0300

    Parser: add function call parsing

---
+++ AST/Nodes/FunctionCall.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <utility>
+
+#include "../ASTNode.h"
+
+namespace oneCC::AST {
+
+class FunctionCallNode : public Node {
+public:
+    FunctionCallNode() : Node(FunctionCallExpression) { }
+    FunctionCallNode(Node* name, std::vector<Node*>  arguments)
+            : Node(FunctionCallExpression)
+            , m_name(name)
+            , m_arguments(std::move(arguments)) { }
+
+    ~FunctionCallNode() override = default;
+
+    void setName(Node* name) { m_name = name; }
+    void setArguments(const std::vector<Node*>& arguments) { m_arguments = arguments; }
+
+    Node* name() const { return m_name; }
+    std::vector<Node*> arguments() const { return m_arguments; }
+
+private:
+    Node* m_name {};
+    std::vector<Node*> m_arguments {};
+};
+
+}
