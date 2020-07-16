177558dcc133c7b6887a19b32b7bb64357a288ec
Plunkerusr <slava.pach@gmail.com>
2020-07-16 16:07:51 +0300

    Parser: support function arguments with default expression

--- AST/Nodes/FunctionArgument.h
+++ AST/Nodes/FunctionArgument.h
@@ -5,17 +5,19 @@ namespace oneCC::AST {
 
 class FunctionArgumentNode : public Node {
 public:
-    FunctionArgumentNode(Lexer::TokenType type, const std::string& name)
+    FunctionArgumentNode(Lexer::TokenType type, const std::string& name, Node* expression)
         : Node(servedType())
         , m_type(type)
         , m_name(name)
+        , m_expression(expression)
     {
     }
 
-    FunctionArgumentNode(Lexer::TokenType type, std::string&& name)
+    FunctionArgumentNode(Lexer::TokenType type, std::string&& name, Node* expression)
         : Node(servedType())
         , m_type(type)
         , m_name(std::move(name))
+        , m_expression(expression)
     {
     }
     
@@ -23,12 +25,14 @@ public:
 
     Lexer::TokenType type() const { return m_type; }
     const std::string& name() const { return m_name; }
+    Node* expression() const { return m_expression; }
 
     static NodeType servedType() { return FunctionArgument; }
 
 private:
     Lexer::TokenType m_type;
     std::string m_name;
+    Node* m_expression;
 };
 
 }
