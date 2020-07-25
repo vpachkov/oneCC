58a6d83ce891b258147fe7649f84b5e58008b936
Plunkerusr <slava.pach@gmail.com>
2020-07-26 01:40:21 +0300

    CodeGenerator: add result register to an expression class

--- src/AST/Nodes/Expression.h
+++ src/AST/Nodes/Expression.h
@@ -9,9 +9,14 @@ public:
     void setExpressionType(Lexer::TokenType expressionType) { m_expressionType = expressionType; }
     Lexer::TokenType expressionType() const { return m_expressionType ; }
 
+    void setResultRegister(int resultRegister) { m_resultRegister = resultRegister; }
+    int resultRegister() const { return m_resultRegister ; }
+
 protected:
     Expression(NodeType type, Lexer::TokenType expressionType) : Node(type), m_expressionType(expressionType) {}
     Lexer::TokenType m_expressionType;
+
+    int m_resultRegister; // indicates which register keeps the result of an expression
 };
 
 }
