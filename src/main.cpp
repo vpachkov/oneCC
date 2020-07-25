97ed25f4ca0e54f1d827e304a22b3d845d086c28
nimelehin <nimelehin@gmail.com>
2020-07-25 09:23:35 +0300

    Main: Clean main

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,18 @@
 #include <iostream>
 #include <vector>
 
+#define DEBUG_TOKINIZE_FILE
+// #define DEBUG_VIZ
+
 inline bool exists_test0(const std::string& name)
 {
     std::ifstream f(name.c_str());
     return f.good();
 }
 
-int kek(){
-    {
-        {
-            return 1;
-        }
-        return 1;
-    }
-}
-
 int main()
 {
+#ifdef DEBUG_TOKINIZE_FILE
     auto ifstreamPtr = std::make_unique<std::ifstream>("demos/example.txt");
     auto lexer = oneCC::Lexer::Lexer(std::move(ifstreamPtr));
 
@@ -43,42 +38,28 @@ int main()
         }
     }
     std::cout << "\n\n";
+#endif // DEBUG_TOKINIZE_FILE
+    
     auto ifstreamPtr4Parser = std::make_unique<std::ifstream>("demos/example.txt");
     auto lexer4Parser = std::make_unique<oneCC::Lexer::Lexer>(std::move(ifstreamPtr4Parser));
-
-    //    try {
-    //        lexer4Parser->tokinizeFile();
-    //    } catch (std::exception& e) {
-    //        std::cout << "\n" << e.what() << "\n";
-    //        return 1;
-    //    }
-
-    //    for (;;) {
-    //        auto token = lexer4Parser->skipToken();
-    //        if (token.type() == oneCC::Lexer::TokenType::EndOfFile) {
-    //            break;
-    //        } else {
-    //            std::cout << token.lexeme() << "(" << oneCC::Lexer::Token::typeToString(token) << ") ";
-    //        }
-    //    }
-    //    std::cout << "\n\n";
-
     auto parser = oneCC::Parser::Parser(std::move(lexer4Parser));
     oneCC::AST::Node* root;
+    
     try {
         root = parser.parse();
     } catch (std::exception& e) {
         std::cout << "\n" << e.what() << "\n";
         return 1;
     }
-
-    auto a = oneCC::SemanticAnalyzer::SemanticAnalyzer().processTree(root);
-
     std::cout << "Parsed\n";
-    std::cout.flush();
+    
+    auto a = oneCC::SemanticAnalyzer::SemanticAnalyzer().processTree(root);
+    std::cout << "Checks passed\n";
 
-     // auto viz = oneCC::ASTUtils::Visualizer();
-     // viz.genTreePng(root);
+#ifdef DEBUG_VIZ
+    auto viz = oneCC::ASTUtils::Visualizer();
+    viz.genTreePng(root);
+#endif // DEBUG_VIZ
 
     return 0;
 }
