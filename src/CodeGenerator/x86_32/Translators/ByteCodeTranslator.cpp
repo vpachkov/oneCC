48cbb4333d74a2c34bff09a4db01d8a25ae82d0e
nimelehin <nimelehin@gmail.com>
2020-07-25 11:46:57 +0300

    CodeGeneration: Add class AbstractX86_32Translator

--- src/CodeGenerator/x86_32/Translators/ByteCodeTranslator.cpp
+++ src/CodeGenerator/x86_32/Translators/ByteCodeTranslator.cpp
@@ -1,16 +1,30 @@
 #include "ByteCodeTranslator.h"
+#include <cassert>
+#include <iostream>
+
+#define NOT_IMPL(x) do { \
+    std::cout << "\033[1;31mNOT IMPLEMENTED\033[0m in \033[1;31m" << __FILE__ << "\033[0m\n"; \
+    assert(0); \
+} while (0)
 
 namespace oneCC::CodeGenerator::X86_32 {
 
-void ByteCodeTranslator::ADD_rm8_reg8(RM op1, Register op2) {}
-void ByteCodeTranslator::ADD_rm32_reg32(RM op1, Register op2) {}
-void ByteCodeTranslator::ADD_reg8_rm8(Register op1, RM op2) {}
-void ByteCodeTranslator::ADD_reg32_rm32(Register op1, RM op2) {}
-void ByteCodeTranslator::ADD_AL_imm8(uint8_t op2) {}
-void ByteCodeTranslator::ADD_EAX_imm32(uint32_t op2) {}
-void ByteCodeTranslator::PUSH_ES() {}
-void ByteCodeTranslator::POP_ES() {}
-void ByteCodeTranslator::OR_rm8_reg8(RM op1, Register op2) {}
-void ByteCodeTranslator::OR_rm32_reg32(RM op1, Register op2) {}
+void ByteCodeTranslator::ADC_rm32_reg32(RM op1, Register op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADC_rm8_reg8(RM op1, Register op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADD_AL_imm8(uint8_t op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADD_EAX_imm32(uint32_t op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADD_reg32_rm32(Register op1, RM op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADD_reg8_rm8(Register op1, RM op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADD_rm32_reg32(RM op1, Register op2) { NOT_IMPL(); }
+void ByteCodeTranslator::ADD_rm8_reg8(RM op1, Register op2) { NOT_IMPL(); }
+void ByteCodeTranslator::OR_AL_imm8(uint8_t op2) { NOT_IMPL(); }
+void ByteCodeTranslator::OR_EAX_imm32(uint32_t op2) { NOT_IMPL(); }
+void ByteCodeTranslator::OR_reg32_rm32(Register op1, RM op2) { NOT_IMPL(); }
+void ByteCodeTranslator::OR_reg8_rm8(Register op1, RM op2) { NOT_IMPL(); }
+void ByteCodeTranslator::OR_rm32_reg32(RM op1, Register op2) { NOT_IMPL(); }
+void ByteCodeTranslator::OR_rm8_reg8(RM op1, Register op2) { NOT_IMPL(); }
+void ByteCodeTranslator::POP_ES() { NOT_IMPL(); }
+void ByteCodeTranslator::PUSH_CS() { NOT_IMPL(); }
+void ByteCodeTranslator::PUSH_ES() { NOT_IMPL(); }
 
 }
--- src/CodeGenerator/x86_32/Translators/ByteCodeTranslator.h
+++ src/CodeGenerator/x86_32/Translators/ByteCodeTranslator.h
@@ -1,26 +1,31 @@
 #pragma once
 
-#include "../Common/Regs.h"
-#include "../Common/ModRM.h"
-#include <cstdint>
+#include "AbstractX86_32Translator.h"
 
 namespace oneCC::CodeGenerator::X86_32 {
 
-class ByteCodeTranslator final {
+class ByteCodeTranslator final : public AbstractX86_32Translator {
 public:
     ByteCodeTranslator() = default;
 
     // Generated (test part)
-    void ADD_rm8_reg8(RM op1, Register op2);
-    void ADD_rm32_reg32(RM op1, Register op2);
-    void ADD_reg8_rm8(Register op1, RM op2);
-    void ADD_reg32_rm32(Register op1, RM op2);
-    void ADD_AL_imm8(uint8_t op2);
-    void ADD_EAX_imm32(uint32_t op2);
-    void PUSH_ES();
-    void POP_ES();
-    void OR_rm8_reg8(RM op1, Register op2);
-    void OR_rm32_reg32(RM op1, Register op2);
+    void ADC_rm32_reg32(RM op1, Register op2) override ;
+    void ADC_rm8_reg8(RM op1, Register op2) override ;
+    void ADD_AL_imm8(uint8_t op2) override ;
+    void ADD_EAX_imm32(uint32_t op2) override ;
+    void ADD_reg32_rm32(Register op1, RM op2) override ;
+    void ADD_reg8_rm8(Register op1, RM op2) override ;
+    void ADD_rm32_reg32(RM op1, Register op2) override ;
+    void ADD_rm8_reg8(RM op1, Register op2) override ;
+    void OR_AL_imm8(uint8_t op2) override ;
+    void OR_EAX_imm32(uint32_t op2) override ;
+    void OR_reg32_rm32(Register op1, RM op2) override ;
+    void OR_reg8_rm8(Register op1, RM op2) override ;
+    void OR_rm32_reg32(RM op1, Register op2) override ;
+    void OR_rm8_reg8(RM op1, Register op2) override ;
+    void POP_ES() override ;
+    void PUSH_CS() override ;
+    void PUSH_ES() override ;
 
 };
 
