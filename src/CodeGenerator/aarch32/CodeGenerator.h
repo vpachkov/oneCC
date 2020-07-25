1dc510528cf76c7fd2713b71bfc38ea5823387a2
nimelehin <nimelehin@gmail.com>
2020-07-25 21:43:46 +0300

    CodeGeneration: Add basic stack operations for aarch32

--- src/CodeGenerator/aarch32/CodeGenerator.h
+++ src/CodeGenerator/aarch32/CodeGenerator.h
@@ -2,20 +2,31 @@
 
 #include "../../AST/AbstractAST.h"
 #include "../../Lexer/Token.h"
+#include "Translators/AsmTranslator.h"
 #include <cstdint>
 #include <iostream>
-#include <map>
 #include <stack>
 #include <string>
+#include <unordered_map>
 
 namespace oneCC::CodeGenerator::Aarch32 {
 
+enum StorageKeys {
+    FUNC_LOCAL_VARS,
+};
+
 class CodeGeneratorAarch32 final : public AST::AbstractAST {
 public:
-    CodeGeneratorAarch32() = default;
+    CodeGeneratorAarch32()
+        : m_translator()
+        , m_storage()
+    {
+    }
+
     int processTree(AST::Node* program);
 
 private:
+
     // From AST::AbstractAST
     using AST::AbstractAST::visitNode;
     void visitNode(AST::BinaryOperationNode* a) override;
@@ -28,6 +39,12 @@ private:
     void visitNode(AST::FunctionNode* a) override;
     void visitNode(AST::FunctionCallNode* a) override;
     void visitNode(AST::ProgramNode* a) override;
+
+    void initStackFrame(AST::FunctionNode* func);
+    void restoreStackFrame(AST::FunctionNode* func);
+
+    AsmTranslator m_translator;
+    std::unordered_map<int, int> m_storage;
 };
 
 }
--- src/CodeGenerator/aarch32/RegisterManager/RegisterManager.h
+++ src/CodeGenerator/aarch32/RegisterManager/RegisterManager.h
@@ -3,10 +3,10 @@
 #include <vector>
 
 
-class RegisterManager {
-public:
-    int allocateRegister();
-    int freeRegister();
-private:
-    std::vector<int>registers {};
-};
+// class RegisterManager {
+// public:
+//     int allocateRegister();
+//     int freeRegister();
+// private:
+//     std::vector<int>registers {};
+// };
