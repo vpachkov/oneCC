4485f042fdd65c32d42c20b8887f112bc36bbe68
nimelehin <nimelehin@gmail.com>
2020-07-26 17:40:08 +0300

    CodeGeneration: Change Regs for aarch32

--- src/CodeGenerator/aarch32/Translators/AbstractAarch32Translator.h
+++ src/CodeGenerator/aarch32/Translators/AbstractAarch32Translator.h
@@ -10,11 +10,11 @@ public:
     AbstractAarch32Translator() = default;
 
     // TODO: Add <q> and <c> from dev arm page.
-    virtual void ADD_imm12(Register Rd, Register Rn, uint16_t imm12) {}
-    virtual void BX(Register Rm) {}
+    virtual void ADD_imm12(Register& Rd, Register& Rn, uint16_t imm12) {}
+    virtual void BX(Register& Rm) {}
     virtual void POP_multiple_registers(RegisterList list) {}
     virtual void PUSH_multiple_registers(RegisterList list) {}
-    virtual void SUB_imm12(Register Rd, Register Rn, uint16_t imm12) {}
+    virtual void SUB_imm12(Register& Rd, Register& Rn, uint16_t imm12) {}
 };
 
 }
--- src/CodeGenerator/aarch32/Translators/AsmTranslator.h
+++ src/CodeGenerator/aarch32/Translators/AsmTranslator.h
@@ -8,11 +8,11 @@ class AsmTranslator final : public AbstractAarch32Translator {
 public:
     AsmTranslator() = default;
 
-    void ADD_imm12(Register Rd, Register Rn, uint16_t imm12) override;
-    void BX(Register Rm) override;
+    void ADD_imm12(Register& Rd, Register& Rn, uint16_t imm12) override;
+    void BX(Register& Rm) override;
     void POP_multiple_registers(RegisterList list) override;
     void PUSH_multiple_registers(RegisterList list) override;
-    void SUB_imm12(Register Rd, Register Rn, uint16_t imm12) override;
+    void SUB_imm12(Register& Rd, Register& Rn, uint16_t imm12) override;
 
     void addLabel(const char* text);
 };
