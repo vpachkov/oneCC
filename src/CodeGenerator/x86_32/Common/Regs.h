b9cb911a46eddd2d69ef66fb72a9316c5f745a33
Plunkerusr <slava.pach@gmail.com>
2020-07-29 03:44:40 +0300

    CodeGenerator: add registerToString function

--- src/CodeGenerator/x86_32/Common/Regs.h
+++ src/CodeGenerator/x86_32/Common/Regs.h
@@ -7,6 +7,21 @@ enum Register {
     EBX,BX,BH,BL,
     ECX,CX,CH,CL,
     EDX,DX,DH,DL,
+
+    ESP, EBP,
 };
+
+static const char* registerToString(Register reg) {
+    static const char* regs[] = {
+        "EAX", "AX", "AH", "AL",
+        "EBX", "BX", "BH", "BL",
+        "ECX", "CX", "CH", "CL",
+        "EDX", "DX", "DH", "DL",
+
+        "ESP", "EBP",
+    };
+
+    return regs[reg];
+}
 
 }
