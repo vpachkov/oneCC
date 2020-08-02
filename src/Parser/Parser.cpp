739fa0a43f1436ac8d47e7659e8769ae87fd2052
nimelehin <nimelehin@gmail.com>
2020-08-02 11:30:29 +0300

    Parser: Move defines out

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -14,24 +14,10 @@
 #include "../AST/Nodes/WhileStatement.h"
 #include "../Exceptions.h"
 #include "../Utils/ASTReader/ASTReader.h"
+#include "Defines.h"
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-
-#define eatToken(...)                       \
-    if (!tryToEatToken(__VA_ARGS__))        \
-        [[unlikely]]                        \
-        {                                   \
-            generateErrorText(__VA_ARGS__); \
-            return NULL;                    \
-        }
-
-#define softAssertNode(x) \
-    if (!x)               \
-        [[unlikely]]      \
-        {                 \
-            return NULL;  \
-        }
 
 namespace oneCC::Parser {
 
